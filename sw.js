d0724262c187749223d78d7ab720c091d3f51004
shang-code <syz15822409222@126.com>
2019-08-27 22:37:27 +0800

    Site updated: 2019-08-27 22:37:26

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/index.html","b386d80d4610d97d19f464f726f82ab3"]];
+var precacheConfig = [["/index.html","0affcba09f0bc635cea017a4461fd629"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
